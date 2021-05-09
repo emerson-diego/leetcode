514603518aa7085a8fa93c25d902349bd0d2f74f
Emerson <diegowebby@gmail.com>
2021-05-09 09:46:12 -0300

    add Valid Parentheses

---
+++ java/ValidParentheses.java
@@ -0,0 +1,64 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String args[]){
+        System.out.println(new ValidParentheses().isValid("{[]}"));
+
+    }
+
+    public boolean isValid(String s) {
+        Stack<String> stk = new Stack<>();  
+        Stack<String> stkResult = recursive(s, stk);
+        if(stkResult.empty()){
+            return true;
+        }
+        else return false;
+    }
+
+    public Stack<String> recursive(String s, Stack stk){
+
+        String character = "";
+        if(s==null || s.equals("")){
+            return stk;
+        }
+        else{
+            character = String.valueOf(s.charAt(0));
+            s = s.substring(1, s.length());
+        }
+
+            if(!stk.empty() && inverse(stk.peek().toString(),character) ){
+                stk.pop();
+                return recursive(s, stk);
+            }
+           // else if(isClosed(character)){
+
+           // }
+            else{
+                stk.push(character);
+                recursive(s, stk);
+            }
+                
+        
+        return stk;
+
+    }
+
+    private boolean inverse(String peek, String s) {
+
+        if(peek.equals("(") && s.equals(")"))
+            return true;
+        if(peek.equals("[") && s.equals("]"))
+            return true;
+        if(peek.equals("{") && s.equals("}"))
+            return true;
+        return false;
+    }
+
+    private boolean isClosed(String s){
+        if(s.equals(")") || s.equals("]") || s.equals("}"))
+            return true;
+        else return false;
+    }
+    
+}
