8f06960eb8d896bfc0c278d26a2672a4a1476a0f
Emerson <diegowebby@gmail.com>
2021-06-03 09:29:48 -0300

    add SearchInsertPosition

--- java/ValidParentheses.java
+++ java/ValidParentheses.java
@@ -2,63 +2,55 @@ import java.util.Stack;
 
 public class ValidParentheses {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println(new ValidParentheses().isValid("{[]}"));
 
     }
 
     public boolean isValid(String s) {
-        Stack<String> stk = new Stack<>();  
-        Stack<String> stkResult = recursive(s, stk);
-        if(stkResult.empty()){
+        Stack<String> stk = new Stack<>();
+        recursive(s, stk);
+        if (stk.empty()) {
             return true;
-        }
-        else return false;
+        } else
+            return false;
     }
 
-    public Stack<String> recursive(String s, Stack stk){
+    public Stack<String> recursive(String s, Stack stk) {
 
         String character = "";
-        if(s==null || s.equals("")){
+        if (s == null || s.equals("")) {
             return stk;
-        }
-        else{
+        } else {
             character = String.valueOf(s.charAt(0));
             s = s.substring(1, s.length());
         }
 
-            if(!stk.empty() && inverse(stk.peek().toString(),character) ){
-                stk.pop();
-                return recursive(s, stk);
-            }
-           // else if(isClosed(character)){
+        if (!stk.empty() && inverse(stk.peek().toString(), character)) {
+            stk.pop();
+            return recursive(s, stk);
+        }
 
-           // }
-            else{
-                stk.push(character);
-                recursive(s, stk);
-            }
-                
-        
+        else {
+            stk.push(character);
+            recursive(s, stk);
+        }
+
         return stk;
 
     }
 
     private boolean inverse(String peek, String s) {
 
-        if(peek.equals("(") && s.equals(")"))
+        if (peek.equals("(") && s.equals(")"))
             return true;
-        if(peek.equals("[") && s.equals("]"))
+        if (peek.equals("[") && s.equals("]"))
             return true;
-        if(peek.equals("{") && s.equals("}"))
+        if (peek.equals("{") && s.equals("}"))
             return true;
         return false;
     }
 
-    private boolean isClosed(String s){
-        if(s.equals(")") || s.equals("]") || s.equals("}"))
-            return true;
-        else return false;
-    }
-    
+
+
 }
