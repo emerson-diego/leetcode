9c61ba6755616fb191de3f8031008cca8224c933
Emerson <diegowebby@gmail.com>
2023-07-23 09:34:20 -0300

    refact: Refact repository

--- src/main/java/P26_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/P26_RemoveDuplicatesFromSortedArray.java
@@ -3,15 +3,6 @@ import java.util.Set;
 
 public class P26_RemoveDuplicatesFromSortedArray {
 
-    public static void main(String[] args) {
-
-        int[] nums = { 1, 1, 2 }; // Input array
-        int[] expectedNums = { 1, 2, 0 }; // The expected answer
-
-        int k = new P26_RemoveDuplicatesFromSortedArray().removeDuplicates(nums); // Calls your implementation
-
-    }
-
     public int removeDuplicates(int[] nums) {
 
         Set<Integer> uniques = new HashSet<>();
